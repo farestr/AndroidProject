a92c33db24d68c858bb9030950213e9476919258
JihadKhader <rashada1004@gmail.com>
2023-07-10 23:52:24 +0300

    V1 - Room reservation + Payment + Confirmation

--- app/src/main/java/firstlook/jihad/zoornahotel/Activity/CreateAccountActivity.java
+++ app/src/main/java/firstlook/jihad/zoornahotel/Activity/CreateAccountActivity.java
@@ -15,11 +15,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.jetbrains.annotations.Nullable;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import firstlook.jihad.zoornahotel.R;
 import firstlook.jihad.zoornahotel.models.User;
@@ -29,10 +41,11 @@ public class CreateAccountActivity extends AppCompatActivity {
     private EditText editTextEmail;
     private EditText editTextPassword;
     private EditText editTextDateOfBirth;
-
     private Spinner spinnerGender;
     private EditText editTextPhone;
     private Button buttonCreateAccount;
+    private String URL = "http://localhost/android/register.php";
+    String name,email,password,dateOfBirth,gender,phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,43 +72,96 @@ public class CreateAccountActivity extends AppCompatActivity {
                 showDatePickerDialog();
             }
         });
-
+//
+//        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String name = editTextName.getText().toString();
+//                String email = editTextEmail.getText().toString();
+//                String password = editTextPassword.getText().toString();
+//                String dateOfBirthStr = editTextDateOfBirth.getText().toString();
+//                String gender = spinnerGender.getSelectedItem().toString();
+//                String phone = editTextPhone.getText().toString();
+//
+//                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) ||
+//                        TextUtils.isEmpty(dateOfBirthStr) || TextUtils.isEmpty(gender) || TextUtils.isEmpty(phone)) {
+//                    Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//
+//                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+//                Date dateOfBirth;
+//                try {
+//                    dateOfBirth = dateFormat.parse(dateOfBirthStr);
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                    Toast.makeText(getApplicationContext(), "Invalid date format", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//
+//                User user = new User(name, email, password, dateOfBirth, gender, phone);
+//
+//                Toast.makeText(getApplicationContext(), "Account created successfully", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
         buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = editTextName.getText().toString();
-                String email = editTextEmail.getText().toString();
-                String password = editTextPassword.getText().toString();
-                String dateOfBirthStr = editTextDateOfBirth.getText().toString();
-                String gender = spinnerGender.getSelectedItem().toString();
-                String phone = editTextPhone.getText().toString();
-
-                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) ||
-                        TextUtils.isEmpty(dateOfBirthStr) || TextUtils.isEmpty(gender) || TextUtils.isEmpty(phone)) {
-                    Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-                Date dateOfBirth;
-                try {
-                    dateOfBirth = dateFormat.parse(dateOfBirthStr);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "Invalid date format", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                User user = new User(name, email, password, dateOfBirth, gender, phone);
-
-                Toast.makeText(getApplicationContext(), "Account created successfully", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                save(v);
             }
         });
     }
 
+    public void save(View view){
+        name = editTextName.getText().toString().trim();
+        email = editTextEmail.getText().toString().trim();
+        password = editTextPassword.getText().toString().trim();
+        dateOfBirth = editTextDateOfBirth.getText().toString().trim();
+        gender = spinnerGender.getSelectedItem().toString().trim();
+        phone = editTextPhone.getText().toString().trim();
+        if(!name.equals("")&& !email.equals("")&&!password.equals("")&&!dateOfBirth.equals("")&&!gender.equals("")&&!phone.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(CreateAccountActivity.this, RoomActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(CreateAccountActivity.this, "Invalid Login email or password", Toast.LENGTH_SHORT);
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(CreateAccountActivity.this,error.toString().trim(),Toast.LENGTH_SHORT);
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> data = new HashMap<>();
+                    data.put("email",name);
+                    data.put("email",email);
+                    data.put("password",password);
+                    data.put("email",dateOfBirth);
+                    data.put("email",gender);
+                    data.put("email",phone);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
+    }
+    public void login(){
+        Intent intent =  new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
     private void showDatePickerDialog() {
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
--- app/src/main/java/firstlook/jihad/zoornahotel/Activity/MainActivity.java
+++ app/src/main/java/firstlook/jihad/zoornahotel/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package firstlook.jihad.zoornahotel.Activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,13 +11,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import firstlook.jihad.zoornahotel.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private String email,password;
     private Button buttonLogin;
     private Button buttonCreateAccount;
+    private String URL = "http://localhost/android/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,46 +42,60 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.buttonLogin);
         buttonCreateAccount = findViewById(R.id.buttonCreateAccount);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+      buttonLogin.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              login(v);
+          }
+      });
+      buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              register(v);
+          }
+      });
+    }
+public void login(View view){
+        email = editTextEmail.getText().toString().trim();
+    password = editTextPassword.getText().toString().trim();
+    if(!email.equals("")&& !password.equals("")){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
-            public void onClick(View v) {
-                String email = editTextEmail.getText().toString();
-                String password = editTextPassword.getText().toString();
-
-                // Validate input
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Please enter email", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                // Perform login authentication here
-                boolean loginSuccessful = performLogin(email, password);
-
-                if (loginSuccessful) {
-                    Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+            public void onResponse(String response) {
+                if (response.equals("success")) {
                     Intent intent = new Intent(MainActivity.this, RoomActivity.class);
                     startActivity(intent);
                     finish();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_SHORT).show();
+                } else if (response.equals("failure")) {
+                    Toast.makeText(MainActivity.this, "Invalid Login email or password", Toast.LENGTH_SHORT);
                 }
             }
-        });
-
-        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+        }, new Response.ErrorListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
-                startActivity(intent);
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString().trim(),Toast.LENGTH_SHORT);
             }
-        });
+        }){
+//            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> data = new HashMap<>();
+                data.put("email",email);
+                data.put("password",password);
+                return data;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }else{
+        Toast.makeText(this,"fileds cannot be empty",Toast.LENGTH_SHORT);
     }
-
+}
+    public void register(View view){
+        Intent intent = new Intent(this,CreateAccountActivity.class);
+        startActivity(intent);
+        finish();
+    }
     // Method to perform login authentication (dummy implementation)
     private boolean performLogin(String email, String password) {
         // Replace this with your actual login authentication logic
